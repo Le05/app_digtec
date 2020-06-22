6951823ea42af4d1a928d6905f8e541e36bb396b
unknown <leosantospereira10@hotmail.com>
2020-06-21 21:31:30 -0300

    correcoes

--- lib/app/modules/tips/widgets/moreTips/moreTips_widget.dart
+++ lib/app/modules/tips/widgets/moreTips/moreTips_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_native_html_view/flutter_native_html_view.dart';
 import 'package:franet/app/modules/tips/widgets/moreTips/moreTips_bloc.dart';
 
